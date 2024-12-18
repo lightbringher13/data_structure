ad301f8ceb0eaaadf6ed6a16aabf6431ff5cb3de
Jeon JaeHwan <lightbringher13@gmail.com>
2024-12-18 14:38:57 +0900

    Add, Rename directory

---
+++ b_before_data_structure/a_what_is_data_structure.py
@@ -0,0 +1,20 @@
+# data structure is data ----> memory + write, read, insert,
+# delete, search
+
+# algorithm = finite operations to find the answer
+# using data structure
+
+# ex) variable
+# a = 5 --> write
+# print(a) --> read
+# in python variable a does not store the value itself
+# it stores the memory address of the object
+
+# ex) array
+# a = [3 -1 5 7]
+# write, read using index
+# a[3] = 3, print(a[4])
+# insert
+# a.append(34)
+# delete
+# a.pop(3),a.pop()
